0ea1ac623143d879bc5d4560b6a0622ae0e004a9
Violanacho <juaniviola1297@gmail.com>
2018-09-14 16:52:08 -0300

    index.js

--- db/index.js
+++ db/index.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const mongoose = require('mongoose')
-const chalk = require('chalk')
-const mongoUrl = 'mongodb://127.0.0.1:27017/tw-clone'
+const api = require('./api')
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-  .then(() => console.log(chalk.green('Conectado a la database')))
-  .catch(err => console.log(chalk.red(`Error: ${err}`)))
+module.exports = {
+  connect (url, port, db) {
+    const mongoUrl = `mongodb://${url}:${port}/${db}`
+    return mongoose.connect(mongoUrl, { useNewUrlParser: true })
+  },
+
+  api
+}
