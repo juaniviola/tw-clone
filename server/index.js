5ff5c1e558eaa5b0d6141462e12ff0dfd792f385
Violanacho <juaniviola1297@gmail.com>
2018-10-28 22:14:07 -0300

    handling fatal errors

--- server/index.js
+++ server/index.js
@@ -18,6 +18,16 @@ if (process.env.NODE_ENV === 'development') {
   app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 }
 
+process.on('uncaughtException', function (err) {
+  console.error(err.message)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', function (err) {
+  console.error(err.message)
+  process.exit(1)
+})
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`${chalk.green('[Server] Listening on port ' + port)}`)
