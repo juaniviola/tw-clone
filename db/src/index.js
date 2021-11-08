ba09741efa73f2e1f303272c7880ca4c53b71233
juaniviola <juaniviola1297@gmail.com>
2021-11-08 18:40:38 -0300

    main function updated

--- db/src/index.js
+++ db/src/index.js
@@ -1,11 +1,6 @@
 import mongoose from 'mongoose';
 import api from './api';
 
-export default function connect(mongoUrl) {
-  const { url, port, db } = mongoUrl;
-  const urldb = `mongodb://${url}:${port}/${db}`;
-
-  return mongoose.connect(urldb, { useNewUrlParser: true });
-}
+export default (uri) => mongoose.connect(uri, { useNewUrlParser: true });
 
 export { api };
