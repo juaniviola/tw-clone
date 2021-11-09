d3ea48f4f6645a5c0d260a4ee49e05dccd04a9be
juaniviola <juaniviola1297@gmail.com>
2021-11-09 20:04:35 -0300

    mongodb memory server for development

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 /* eslint-disable no-console */
 import express from 'express';
 import http from 'http';
 import helmet from 'helmet';
 import cors from 'cors';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import server from './graphql';
+import database from './graphql/resolvers/Database';
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,9 +23,17 @@ process.on('unhandledRejection', (err) => {
 });
 
 async function runServer() {
+  const envNode = process.env.ENV === 'env';
   const app = express();
   app.use(helmet());
   app.use(cors());
+
+  const memoryDb = envNode ? Promise.resolve(MongoMemoryServer.create()) : null;
+  const uri = envNode ? (await memoryDb).getUri() : process.env.URL_DB;
+
+  await database.connect(uri);
+  if (database.connected) console.log('Database connected...');
+  else console.error('Database not connected...');
 
   await server.start();
   server.applyMiddleware({
@@ -34,4 +47,4 @@ async function runServer() {
   console.log(`🚀 Server ready at http://localhost:${PORT}`);
 }
 
-runServer();
+runServer().catch(err => console.error(err));
