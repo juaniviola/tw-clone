982e5794d86d5c0131844c5683f18d0fdbc095d2
juaniviola <juaniviola1297@gmail.com>
2021-11-11 18:26:05 -0300

    Resolver User, Tweet, Answer. return _id as string

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,12 +1,24 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 import { ApolloServer } from 'apollo-server-express';
 import schema from './schema';
 import queryResolvers from './resolvers';
+import db from './resolvers/Database';
 
 export default new ApolloServer({
   typeDefs: schema,
   resolvers: {
     Query: {
       ...queryResolvers,
+    },
+    User: {
+      _id: ({ _id }) => db.Utils.objectIdToString(_id),
+    },
+    Tweet: {
+      _id: ({ _id }) => db.Utils.objectIdToString(_id),
+    },
+    Answer: {
+      _id: ({ _id }) => db.Utils.objectIdToString(_id),
     },
   },
 });
