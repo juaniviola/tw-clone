e98d7b10bcdfc280b0827b2c33c6882a24c84a85
juaniviola <juaniviola1297@gmail.com>
2021-11-11 18:26:43 -0300

    user query

--- server/graphql/resolvers/Query/User.js
+++ server/graphql/resolvers/Query/User.js
@@ -1,14 +1,26 @@
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable import/no-named-as-default-member */
 import db from '../Database';
-import wrapAsync from '../modules';
+import { wrapAsync } from '../modules';
 
 const Querys = {
   hello: (_, { name }) => `Hello ${name || 'World'}`,
-  userById: (_, { id }) => wrapAsync(db.User.getById, id),
-  userByUsername: (_, { username }) => wrapAsync(db.User.getByUsername, username),
-  usersByUsername: (_, { username }) => wrapAsync(db.User.getUsersByUsername, username),
-  userFollowers: (_, { id }) => wrapAsync(db.User.getFollowers, id),
+  userById: async (_, { id }) => {
+    const query = await wrapAsync(db.User.getById, id);
+    return query;
+  },
+  userByUsername: async (_, { username }) => {
+    const query = await wrapAsync(db.User.getByUsername, username);
+    return query;
+  },
+  usersByUsername: async (_, { username }) => {
+    const query = await wrapAsync(db.User.getUsersByUsername, username);
+    return query;
+  },
+  userFollowers: async (_, { id }) => {
+    const query = await wrapAsync(db.User.getFollowers, id);
+    return query;
+  },
 };
 
 export default Querys;
