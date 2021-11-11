c2330db049f2f654e0e868134b94c9f6f03ac783
juaniviola <juaniviola1297@gmail.com>
2021-11-11 18:26:35 -0300

    tweet query

--- server/graphql/resolvers/Query/Tweet.js
+++ server/graphql/resolvers/Query/Tweet.js
@@ -1,11 +1,38 @@
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable import/no-named-as-default-member */
 import db from '../Database';
-import wrapAsync from '../modules';
+import { wrapAsync, verifyToken } from '../modules';
+import config from '../../../config';
+
+const { SECRET_TOKEN } = config;
 
 const Querys = {
-  tweetById: (_, { id }) => wrapAsync(db.Tweet.getById, id),
-  tweetsByHashtag: (_, { hashtag }) => wrapAsync(db.Tweet.getByHashtags, hashtag),
+  tweetById: async (_, { id }) => {
+    const query = await wrapAsync(db.Tweet.getById, id);
+    return query;
+  },
+  tweetsByHashtag: async (_, { hashtag }) => {
+    const query = await wrapAsync(db.Tweet.getByHashtags, hashtag);
+    return query;
+  },
+  tweetsByUser: async (_, { id }) => {
+    const query = await wrapAsync(
+      db.Tweet.getByUser,
+      wrapAsync(db.Utils.stringToObjectId, id),
+    );
+    return query;
+  },
+  tweetsByFollowingUsers: async (_, { token }) => {
+    try {
+      const decodeToken = await verifyToken(token, SECRET_TOKEN);
+      const { iss } = decodeToken;
+
+      const tweets = await db.Tweet.tweetByFollowingUsers({ id: iss });
+      return tweets;
+    } catch (error) {
+      return null;
+    }
+  },
 };
 
 export default Querys;
