706a78470478b4bcc5812bc5b837ba87eaed5589
juaniviola <juaniviola1297@gmail.com>
2021-11-15 20:03:47 -0300

    added user mutations

--- server/graphql/resolvers/Mutation/User.js
+++ server/graphql/resolvers/Mutation/User.js
@@ -11,6 +11,40 @@ const Mutations = {
       return null;
     }
   },
+
+  addFollow: async (_, { userId }, { userToken }) => {
+    try {
+      if (!userToken) throw Error(0);
+
+      const followAdd = await db.User.addFollower({
+        userFromId: db.Utils.stringToObjectId(userToken),
+        userToId: db.Utils.stringToObjectId(userId),
+      });
+
+      if (!followAdd[0] || !followAdd[1]) throw Error(0);
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  },
+
+  deleteFollow: async (_, { userId }, { userToken }) => {
+    try {
+      if (!userToken) throw Error(0);
+
+      const delFollow = await db.User.deleteFollower({
+        userFromId: db.Utils.stringToObjectId(userToken),
+        userToId: db.Utils.stringToObjectId(userId),
+      });
+
+      if (!delFollow[0] || !delFollow[1]) throw Error(0);
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  },
 };
 
 export default Mutations;
