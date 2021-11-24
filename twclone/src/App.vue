3bcf6297e64ed2013f29737749ec023d994df9b1
Juani Viola <juaniviola1297@gmail.com>
2021-11-24 19:55:03 -0300

    styles deleted from app.vue

--- twclone/src/App.vue
+++ twclone/src/App.vue
@@ -1,30 +1,12 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Lato:wght@300;400&display=swap');
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  html, body {
+    height: 100%;
+    margin: 0;
   }
-}
 </style>
