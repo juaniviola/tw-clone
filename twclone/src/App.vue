80e3fa4430e08a01d53762a441e5c554077816d9
Juani Viola <juaniviola1297@gmail.com>
2021-11-29 18:06:38 -0300

    App.vue event bus listener

--- twclone/src/App.vue
+++ twclone/src/App.vue
@@ -1,6 +1,32 @@
 <template>
-  <router-view/>
+  <div>
+    <Header v-if="userLogged" />
+    <router-view/>
+  </div>
 </template>
+
+<script>
+import Header from '@/components/global/Header.vue';
+import eventBus from '@/utils/EventBus';
+
+export default {
+  data() {
+    return {
+      userLogged: false,
+    };
+  },
+
+  components: {
+    Header,
+  },
+
+  mounted() {
+    eventBus.on('user_logged', (isLogged) => {
+      this.userLogged = isLogged;
+    });
+  },
+};
+</script>
 
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Lato:wght@300;400&display=swap');
