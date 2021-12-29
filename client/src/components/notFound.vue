7e9b681194dddb2b96104299b80271f087a82d95
Juani Viola <juaniviola1297@gmail.com>
2021-12-29 14:16:30 -0300

    Oldest client version deleted. (twclone is newest)

--- client/src/components/notFound.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1 style="margin-top: 35px;">404 <br> Page not found</h1>
-    <v-btn flat @click="$router.push({ name: 'home' })" style="margin-top: 20px;">GO HOME</v-btn>
-  </div>
-</template>
