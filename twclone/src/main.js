980ad3a5d034da24e64f21733cd380a77fc46cbf
Juani Viola <juaniviola1297@gmail.com>
2022-01-10 11:35:15 -0300

    service worker deleted

--- twclone/src/main.js
+++ twclone/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import apolloProvider from './utils/ApolloProvider';
 import App from './App.vue';
-import './registerServiceWorker';
 import router from './router';
 
 createApp(App)
